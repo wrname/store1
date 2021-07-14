33fae92b1ac99d7c932d52b78cc9c17ddc212332
wrname <87291084+wrname@users.noreply.github.com>
2021-07-14 15:00:42 +0800

    Add files via upload

---
+++ day02.py
@@ -0,0 +1,52 @@
+
+
+# python3和python2之间的区别
+# 1）	核心类差异
+# 2）	废弃类差异
+# 3）	修改类差异
+# 4）	第三方工具包差异
+# 5）	工具安装安装
+# 6）	切换
+
+
+
+#a,b两个值交换
+# a = 56
+# b = 78
+# he = a+b
+# a = he - a
+# b = he - b
+# print('a =',a,'b =',b)
+
+
+#下列变量命名是否合法
+# 标识符	是否合法	标识符	是否合法
+# char	否	     Cy%ty	否
+# Oax_li	是	     $123	否
+# fLul	是	      3_3 	否
+# BYTE    否	      T_T   否
+
+
+#打印两个变量值的和
+# stu1 = 45
+# stu2 = 23
+# num = stu1 + stu2
+# print(num)
+
+# #求五个数的总和和平均值
+# a = 23
+# b = 32
+# c = 34
+# d = 43
+# e = 52
+# he = a + b + c + d + e
+# pingjun = he / 5
+# print('总和是',he)
+# print('平均值是',pingjun)
+
+#num1的值，赋给num2
+# num1 = 45
+# num2 = num1
+# print(num2)
+
+
